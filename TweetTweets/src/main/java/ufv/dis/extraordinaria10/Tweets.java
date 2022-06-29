517d145fa6187869e66de6ea2caf1067828ada64
Veronica <veronicaciancia@gmail.com>
2022-06-29 14:47:28 +0200

    Class Tweets

---
+++ TweetTweets/src/main/java/ufv/dis/extraordinaria10/Tweets.java
@@ -0,0 +1,22 @@
+package ufv.dis.extraordinaria10;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Tweets {
+
+    private Collection<Tweet> tweets;
+
+    public Tweets() {
+        this.tweets = new ArrayList<>();
+    }
+
+    public Collection<Tweet> getTweets() {
+        return tweets;
+    }
+
+    public boolean addTweets(Tweet nuevo) {
+        tweets.add(nuevo);
+        return true;
+    }
+}
